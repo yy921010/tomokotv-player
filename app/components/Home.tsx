06a0e75c71dc57b9bf462910081d6c5967ad1f47
charles <charlesmaxwellyoung@gmail.com>
2020-08-02 22:53:49 +0800

    ✨feat: 新增videojs

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,32 +1,29 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React, { useRef, useEffect } from 'react';
-import Hls from 'hls.js';
+import Videojs from 'video.js';
 import styles from './Home.css';
 import PlayerBar from './PlayerBar';
 import PlayerController from './PlayControl';
+import '@videojs/http-streaming';
 
 export default function Home(): JSX.Element {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  const hlsPlayer = () => {
-    const videoSrc = 'http://117.169.120.140:8080/live/cctv-1/.m3u8';
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(videoSrc);
-      hls.attachMedia(videoRef.current as HTMLVideoElement);
-      hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        if (videoRef && videoRef.current) {
-          videoRef.current.play();
-        }
-      });
-      hls.on(Hls.Events.ERROR, () => {
-        console.log('ssss');
-      });
-    }
+  const intiPlayer = () => {
+    const player = Videojs(videoRef.current, {}, () => {
+      player.play();
+    });
+    player.src({
+      src: 'http://117.169.120.140:8080/live/cctv-1/.m3u8',
+      type: 'application/x-mpegURL',
+    });
+    setInterval(() => {
+      console.log('player.duration()', player.currentTime());
+    }, 1000);
   };
 
   useEffect(() => {
-    hlsPlayer();
+    intiPlayer();
   }, []);
 
   return (
@@ -39,6 +36,9 @@ export default function Home(): JSX.Element {
         title="Modern web html5 Video player"
         subTitle="subtitle test video"
       />
+      <div data-vjs-player>
+        <video ref={videoRef} className="video-js" width="100%" height="100%" />
+      </div>
     </div>
   );
 }
--- app/components/PlayControl/index.tsx
+++ app/components/PlayControl/index.tsx
@@ -10,6 +10,21 @@ const PlayerController = ({ title, subTitle }: IPlayerControl) => {
   useEffect(() => {}, []);
   return (
     <div className={styles.playerController}>
+      <div className="playerControllerBar" />
+      <div className="playerActions">
+        <div className="volume" />
+        <div className="buttons">
+          <div className="prevSwitch" />
+          <div className="playButton" />
+          <div className="nextSwitch" />
+        </div>
+        <div className="right">
+          <div className="infos" />
+          <div className="menus" />
+          <div className="definitions" />
+          <div className="switchScreen" />
+        </div>
+      </div>
       {title}
       {subTitle}
     </div>
